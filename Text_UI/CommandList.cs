1d9abd5808c42bc52528e6fcaefcbc94f1faf91d
Wiktor <58635705+wiQ1999@users.noreply.github.com>
2020-03-10 12:07:12 +0100

    Added list of comand

--- Text_UI/CommandList.cs
+++ Text_UI/CommandList.cs
@@ -4,22 +4,68 @@ using System.Text;
 
 namespace Text_UI
 {
+    struct Command
+    {
+        public string _sCurrentWord;
+        //public string _sLastWord;
+        public List<string> _oNextWord;
+        public int _iMinNumber;
+        public int _iMaxNumber;
+    }
+
     class CommandList
     {
-        public Dictionary<string, List<string>> Commands {get;set;}
+        public List<Command> Commands { get; set; }
 
 
-    /*
-        Dictionary<string, List<string>> orders1 = new Dictionary<string, List<string>>();
-        orders1.Add("BUY", new List<string>() { "LVL", "TIER", "HP" });
-            orders1.Add("SELL", new List<string>() { "LVL", "TIER" });
-            orders1.Add("SPEED", new List<string>() { "1", "2", "3" });
-            orders1.Add("START", new List<string>() { });
+        public CommandList()
+        {
+            //Wyraz 1
+            this.Commands.Add(new Command()
+            {
+                _sCurrentWord = "BUY",
+                _oNextWord = new List<string>() { "TOWER", "LEVEL", "HP", "CAR", "SKILL" }
+            });
+            this.Commands.Add(new Command()
+            {
+                _sCurrentWord = "SELL", 
+                _oNextWord = new List<string>() { "TOWER", "LEVEL", "CAR" }
+            });
 
-            Dictionary<string, List<int>> orders2 = new Dictionary<string, List<int>>();
-orders2.Add("LVL", new List<int> { 1, 2, 3, 4, 5 });
-            orders2.Add("TIER", new List<int> { 1, 2, 3, 4, 5 });
-            orders2.Add("HP", new List<int> { 10, 20, 30, 40, 50, 60, 70, 80, 90 });
-            */
-	}
+            //Wyraz 2
+            this.Commands.Add(new Command()
+            {
+                _sCurrentWord = "TOWER", 
+                _iMinNumber = 1, 
+                _iMaxNumber = 3
+            });
+            this.Commands.Add(new Command()
+            {
+                _sCurrentWord = "LEVEL",
+                _iMinNumber = 1,
+                _iMaxNumber = 8
+            });
+            this.Commands.Add(new Command()
+            {
+                _sCurrentWord = "HP",
+                _iMinNumber = 1,
+                _iMaxNumber = 100
+            });
+            this.Commands.Add(new Command()
+            {
+                _sCurrentWord = "CAR",
+                _oNextWord = new List<string>() { "FORD", "FIAT", "MERCEDES", "VOLKSWAGEN" }
+            });
+            this.Commands.Add(new Command()
+            {
+                _sCurrentWord = "SKILL",
+                _oNextWord = new List<string>() { "FOOTBALL", "PROGRAMMING", "TREATMENT" }
+            });
+
+            //Wyraz 3
+
+        }
+
+
+    }
 }
